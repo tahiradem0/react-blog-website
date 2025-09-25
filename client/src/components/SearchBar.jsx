2d6f3fb46163f6eae3efaaac927d5c0e2058df13
Tahiradem <ademt0614@gmail.com>
2025-09-25 11:12:21 +0300

    Initial commit: MERN blog project update -13

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,41 +1,30 @@
 import React from 'react';
-import './SocialShare.css';
+import './SearchBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const SocialShare = ({ url, title, description, image }) => {
+
+const SearchBar = ({ searchTerm, onSearchChange }) => {
   return (
-    <div className="social-share">
-      {/* Show blog image at the top if available */}
-      {image && (
-        <div className="social-share-image">
-          <img src={image} alt={title} className="share-image" />
-        </div>
+    <div className="search-bar">
+      <div className="search-icon"><FontAwesomeIcon icon={faSearch} className='serach_icon'/></div>
+      <input
+        type="text"
+        placeholder="Search blogs..."
+        value={searchTerm}
+        onChange={(e) => onSearchChange(e.target.value)}
+        className="search-input"
+      />
+      {searchTerm && (
+        <button 
+          className="clear-search"
+          onClick={() => onSearchChange('')}
+        >
+          ✕
+        </button>
       )}
-
-      <div className="social-share-content">
-        <h4 className="share-title">{title}</h4>
-        <p className="share-description">{description}</p>
-
-        <div className="share-buttons">
-          <a
-            href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="share-btn fb"
-          >
-            Share on Facebook
-          </a>
-          <a
-            href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${encodeURIComponent(title)}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="share-btn tw"
-          >
-            Share on Twitter
-          </a>
-        </div>
-      </div>
     </div>
   );
 };
 
-export default SocialShare;
+export default SearchBar;
